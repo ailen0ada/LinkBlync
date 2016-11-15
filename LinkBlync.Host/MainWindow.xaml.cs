e27c0d215e54d6575f2ff5ce2e144d58e90577a1
Tsubasa HIRANO <ailen0ada@lapin.tokyo>
2016-11-15 23:13:16 +0900

    First commit.

--- LinkBlync.Host/MainWindow.xaml.cs
+++ LinkBlync.Host/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +15,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Blynclight;
+using Microsoft.ServiceBus.Messaging;
 
 namespace LinkBlync.Host
 {
@@ -20,9 +25,69 @@ namespace LinkBlync.Host
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly BlynclightController controller = new BlynclightController();
+
+        private int numberOfDevices;
+        
+        private const string ConnectionString = "primarykey";
+
+        private QueueClient client;
+
+        private IDisposable topicSubscriber;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+            numberOfDevices = controller.InitBlyncDevices();
+            if (numberOfDevices < 1)
+            {
+                MessageBox.Show("No Blynclight device was found.");
+                Environment.Exit(1);
+            }
+
+            controller.ResetLight(0);
+            StatusLabel.Text = controller.aoDevInfo[0].szDeviceName;
+            
+            controller.TurnOnGreenLight(0);
+            
+            client = QueueClient.CreateFromConnectionString(ConnectionString);
+
+            topicSubscriber = Observable.Timer(TimeSpan.Zero, TimeSpan.FromMilliseconds(100))
+                .Select(_ => client.Receive())
+                .Where(msg => msg != null)
+                .Timestamp()
+                .Do(t => Dispatcher.Invoke(() => StatusLabel.Text = $"{t.Timestamp.LocalDateTime:yyyy/MM/dd HH:mm:ss}"))
+                .Select(t =>
+                {
+                    t.Value.Complete();
+                    return t.Value.GetBody<Color>();
+                })
+                .Catch(Observable.Return(Color.FromArgb(0xff, 0xff, 0x00, 0x00)))
+                .Subscribe(c =>
+                {
+                    Dispatcher.Invoke(() => CurrentColorRect.Background = new SolidColorBrush(c));
+                    controller.TurnOnRGBLights(0, c.R, c.G, c.B);
+                });
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            topicSubscriber?.Dispose();
+            client.Close();
+            controller.ResetLight(0);
+            controller.CloseDevices(numberOfDevices);
+            base.OnClosing(e);
+        }
+
+        private async void ResetButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            var message = new BrokeredMessage(Color.FromArgb(0xff, 0xff, 0xff, 0xff));
+            await client.SendAsync(message);
         }
     }
 }
