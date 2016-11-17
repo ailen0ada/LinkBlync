631aedd195c525138d2209f812ad4b787f8aa695
Tsubasa HIRANO <hirano@cuminas.jp>
2016-11-17 20:42:13 +0900

    Add Android project

---
+++ LinkBlync.Client.Droid/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.Widget;
+using Android.OS;
+
+namespace LinkBlync.Client.Droid
+{
+    [Activity(Label = "LinkBlync.Client.Droid", MainLauncher = true, Icon = "@drawable/icon", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
